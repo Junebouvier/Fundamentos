36e31a2588e963c74c3485ba17adad7917b06fea
Isabella Martínez <63332091+Junebouvier@users.noreply.github.com>
2020-07-16 15:10:46 -0500

    Add files via upload

---
+++ Cálculo de cuota moderadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cálculo_de_cuota_moderadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario actual: ");
+            double sal = double.Parse(Console.ReadLine());
+
+            int smmlv = 877803;
+
+            if (sal < (smmlv * 2))
+            {
+                Console.WriteLine("A usted le corresponde la tarifa A. El valor de cuota moderadora para su cita es de $3400");
+            }
+
+            else if (smmlv * 2 <= sal && sal < smmlv * 5)
+            {
+
+                Console.WriteLine("A usted le corresponde la tarifa B. El valor de cuota moderadora para su cita es de $13500");
+            }
+
+            else
+            {
+                Console.WriteLine("A usted le corresponde la tarifa C. El valor de cuota moderadora para su cita es de $35600");
+            }
+        }
+    }
+}
---
+++ Cálculo de tarifa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cálculo_de_tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario actual: ");
+            double sal = double.Parse(Console.ReadLine());
+
+            int smmlv = 877803;
+
+            if (sal < (smmlv * 2))
+            {
+                Console.WriteLine("A usted le corresponde la tarifa A");
+            }
+
+            else if (smmlv * 2 <= sal && sal < smmlv * 4) {
+
+                Console.WriteLine("A usted le corresponde la tarifa B");
+            }
+
+            else
+            {
+                Console.WriteLine("A usted le corresponde la tarifa C");
+            }
+        }
+    }
+}
