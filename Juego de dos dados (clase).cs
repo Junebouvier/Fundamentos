083a406b95d2eab70a0a7dd64f6f7f151b8cb440
Isabella Martínez <63332091+Junebouvier@users.noreply.github.com>
2020-08-11 09:50:32 -0500

    Update Juego de dos dados (clase).cs

--- Juego de dos dados (clase).cs
+++ Juego de dos dados (clase).cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Juego_de_dos_dados__clase_
 {
@@ -8,7 +8,7 @@ namespace Juego_de_dos_dados__clase_
         {
             Console.WriteLine("Bienvenido al juego");
             Random aleatorio = new Random();
-            int dado = 0, dado2 = 0, total = 0, totalAcum = 0, i = 0, turn = 0;
+            int dado = 0, dado2 = 0, total = 0, totalAcum = 0, i = 0, turn = 0, consec = 0;
             
             string continuar = "s";
 
@@ -23,27 +23,37 @@ namespace Juego_de_dos_dados__clase_
                 totalAcum += dado + dado2;
                 Console.WriteLine("Total: " + totalAcum);
                 i++;
+
+                if (dado == dado2) consec++;
+
+                else consec = 0;
+
+                if (consec == 3)
+                {
+                   Console.WriteLine("Felicitaciones, ha ganado el juego");
+                    break;
+                }
                 
                 if (total > 6) turn++;
 
                 if (dado == 1 && dado2 == 1)
-                    {
-                        totalAcum = 0;
-                        Console.WriteLine("Usted ha sido eliminado");
-                        break;
-                    }
+                {
+                    totalAcum = 0;
+                    Console.WriteLine("Usted ha sido eliminado");
+                    break;
+                }
 
-                    else if (totalAcum >= 100)
-                    {
-                        Console.WriteLine("Felicitaciones, ha ganado el juego");
-                        break;
-                    }
+                else if (totalAcum >= 100)
+                {
+                    Console.WriteLine("Felicitaciones, ha ganado el juego");
+                    break;
+                }
 
-                    else
-                    {
-                        Console.WriteLine("¿Desea continuar? (s/n): ");
-                        continuar = (Console.ReadLine());
-                    }
+                else
+                {
+                    Console.WriteLine("¿Desea continuar? (s/n): ");
+                    continuar = (Console.ReadLine());
+                }
 
             }
 
@@ -54,3 +64,4 @@ namespace Juego_de_dos_dados__clase_
     
     }
 }
+
