7e1930e1bd88493e132ac8bbf4707e177e89bb04
Isabella Martínez <63332091+Junebouvier@users.noreply.github.com>
2020-08-18 12:35:56 -0500

    Add files via upload

---
+++ Euler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            double resultado = 0;
+            Console.Write("Ingrese el exponente x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            static double Factorial(double valor)
+            {
+                if (valor <= 1) return 1;
+                return valor * Factorial(valor - 1);
+            }
+
+            for (double i = 0; i <= n; i++)
+            {
+                resultado += (Math.Pow(x, i)) / Factorial(i);
+            }
+
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+
+        
+
+    }
+}
---
+++ Sin(x).cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sin_x_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            double resultado = 0;
+            Console.Write("Ingrese el valor de x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            static double Factorial(double valor)
+            {
+                if (valor <= 1) return 1;
+                return valor * Factorial(valor - 1);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                double denom = 2 * i + 1;
+                resultado += Math.Pow(-1, i) / Factorial(denom) * Math.Pow(x, denom);
+
+            }
+
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
